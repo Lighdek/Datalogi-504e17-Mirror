9265a8ab26d2d98adfc796093eaa60639ab6c984
xiazee <Xiazee@users.noreply.github.com>
2017-11-20 11:15:24 +0100

    Added 2 empty lines between functions Fix spaces and newline at EOF

--- activation_functions.py
+++ activation_functions.py
@@ -3,14 +3,18 @@ import math
 
 euler = 2.7182818284590452353602874713527
 
+
 def relu(x):
-    return max(0,x)
+    return max(0, x)
+
 
 def sigmod(x):
-    return 1/1+ math.pow(euler,-x)
+    return 1/1 + math.pow(euler, -x)
+
 
 def tanh(x):
-    return pow(euler,x)-pow(euler,-x) / pow(euler,x) + pow(euler,-x)
+    return pow(euler, x)-pow(euler, -x) / pow(euler, x) + pow(euler, -x)
+
 
 def l_relu(x,a):
     if x >= 0:
