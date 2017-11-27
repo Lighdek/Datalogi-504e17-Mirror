c4aeebb930e3669b5dd39c6b688793f21319aeda
xiazee <Xiazee@users.noreply.github.com>
2017-11-27 11:27:39 +0100

    WIP

--- TensorModel/JoakimModel.py
+++ TensorModel/JoakimModel.py
@@ -1,69 +1,53 @@
-import tensorflow as flow
-import LayerFunctions as layer
-import activationfunctions as actiavation
+import tensorflow as tf
+from . import Layers
+from . import ActivationFunctions
 
-def init_model(x_image, conf):
+stride = [1, 1, 1, 1]  # input shape.
+stddev = 0.5
 
-    # Convolutional Layer 1
-    layer_conv1, weights_conv1 = layer.conv_layer(input_=x_image,
-                                                  num_of_inputs=3,
-                                                  filter_size=5,
-                                                  num_of_filters=6,
-                                                  name="conv1",
-                                                  stride=conf.stride,
-                                                  distribution=conf.dist)
+# pool
+pool_k_size = [1, 2, 2, 1]
+image_size = 28
+pool_stride = [1, 2, 2, 1]
 
-    # Pooling Layer 1
-    layer_pool1 = layer.max_pooling_layer(input_=layer_conv1,
-                                          name="pool1",
-                                          stride=conf.pool_stride,
-                                          ksize=conf.k_size)
 
-    # RelU layer 1
-    layer_relu1 = actiavation.relu_layer(input_=layer_pool1,
-                                         name="relu1")
+def init_model(input_tensor):
 
-    # -----------------------------------------------------------------------------------
+    # 1
+    layer1_conv = Layers.conv_layer(input_tensor=input_tensor, input_depth=3, filter_size=5,
+                                                   output_depth=6, name="conv1", strides=stride, stddev=stddev)
+    layer1_pool = Layers.max_pooling_layer(input_tensor=layer1_conv, name="pool1", stride=pool_stride,
+                                           ksize=pool_k_size)
+    layer1_relu = ActivationFunctions.relu_layer(input_=layer1_pool, name="relu1")
 
-    # Convolutional Layer 2
-    layer_conv2, weights_conv2 = layer.conv_layer(input_=layer_relu1,
-                                                  num_of_inputs=6,
-                                                  filter_size=5,
-                                                  num_of_filters=16,
-                                                  name="conv2",
-                                                  stride=conf.stride,
-                                                  distribution=conf.dist)
-
-    # Pooling Layer 2
-    layer_pool2 = layer.max_pooling_layer(input_=layer_conv2,
-                                          name="pool2",
-                                          stride=conf.pool_stride,
-                                          ksize=conf.k_size)
-
-    # RelU layer 2
-    layer_relu2 = actiavation.relu_layer(input_=layer_pool2,
-                                          name="relu2")
+    # 2
+    layer2_conv = Layers.conv_layer(input_tensor=layer1_relu, input_depth=6, filter_size=5,
+                                    output_depth=16, name="conv2", strides=stride, stddev=stddev)
+    layer2_pool = Layers.max_pooling_layer(input_tensor=layer2_conv, name="pool2", stride=pool_stride,
+                                           ksize=pool_k_size)
+    layer2_relu = ActivationFunctions.relu_layer(input_=layer2_pool,
+                                                 name="relu2")
     # -----------------------------------------------------------------------------------
 
     # Flatten Layer
-    num_features = layer_relu2.get_shape()[1:4].num_elements()
-    layer_flat = flow.reshape(layer_relu2, [-1, num_features])# -1 finds size on run time
+    num_features = layer2_relu.get_shape()[1:4].num_elements()
+    layer_flat = tf.reshape(layer2_relu, [-1, num_features])# -1 finds size on run time
 
     # Fully-Connected Layer 1
-    layer_fc1 = layer.fully_connected_layer(input_=layer_flat,
-                                            num_of_input=num_features,
-                                            num_of_outputs=128,
-                                            name="fc1",
-                                            distribution=conf.dist)
+    layer_fc1 = Layers.fully_connected_layer(input_tensor=layer_flat,
+                                             num_of_input=num_features,
+                                             num_of_outputs=128,
+                                             name="fc1",
+                                             distribution=stddev)
 
     # RelU layer 3
-    layer_relu3 = actiavation.relu_layer(layer_fc1, name="relu3")
+    layer_relu3 = ActivationFunctions.relu_layer(layer_fc1, name="relu3")
 
     # Fully-Connected Layer 2
-    layer_fc2 = layer.fully_connected_layer(input_=layer_relu3,
-                                            num_of_input=128,
-                                            num_of_outputs=2,
-                                            name="fc2",
-                                            distribution=conf.dist)
+    layer_fc2 = Layers.fully_connected_layer(input_tensor=layer_relu3,
+                                             num_of_input=128,
+                                             num_of_outputs=2,
+                                             name="fc2",
+                                             distribution=stddev)
     return layer_fc2
 
