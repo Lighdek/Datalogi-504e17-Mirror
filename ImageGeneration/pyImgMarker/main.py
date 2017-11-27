90b09d7e3d4a662d141277a7920b4890342dfc8a
Asger Weirsøe <GeneralDenmark@users.noreply.github.com>
2017-11-27 09:55:19 +0100

    Added program to mark 4 corors on picture, so generator knows where to place a licenceplate

---
+++ ImageGeneration/pyImgMarker/main.py
@@ -0,0 +1,16 @@
+from image_handler import main_imgs
+from webbrowser import open
+from traceback import print_exc
+
+settings_path = "imgSettings.JSON"
+image_root = "images"
+
+main_imgs(settings_path, image_root)
+
+
+try:
+   pass
+except BaseException as e:
+    open("http://stackoverflow.com/search?q=[python]+" + str(e) , new=2, autoraise=True)
+    print_exc()
+
