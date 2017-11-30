da1b9081537ebf5e47cd68cc2d12e8bad209cd32
xiazee <Xiazee@users.noreply.github.com>
2017-11-30 13:54:42 +0100

    Probably working model Changes to Generator

--- KerasModel/TestModel.py
+++ KerasModel/TestModel.py
@@ -1,30 +1,94 @@
+import os
+
+from PIL import Image
 from keras import optimizers
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPool2D, Dense, Flatten
+from ImageGeneration import Generator as ImageGenerator
+import numpy as np
+
+from help_functions import print_picture, loadImageMatrix
 
 model = Sequential([
-    Conv2D(filters=8, kernel_size=3, activation='relu', input_shape=(128, 128, 3)),
-    MaxPool2D(),
-    Conv2D(filters=16, kernel_size=3, activation='relu'),
-    MaxPool2D(),
-    Conv2D(filters=32, kernel_size=3, activation='relu'),
-    MaxPool2D(),
-    Conv2D(filters=32, kernel_size=3, activation='relu'),
-    MaxPool2D(),
-    Conv2D(filters=32, kernel_size=3, activation='relu'),
-    MaxPool2D(),
-    Conv2D(filters=32, kernel_size=3, activation='relu'),
-    MaxPool2D(),
-    Conv2D(filters=32, kernel_size=3, activation='relu'),
-    MaxPool2D(),
-    Conv2D(filters=32, kernel_size=3, activation='relu'),
-    MaxPool2D(),
+
+    Conv2D(filters=8, kernel_size=5, activation='relu', padding='same', input_shape=(256, 256, 3),
+           kernel_initializer='random_uniform',
+           bias_initializer='zeros'
+           ),
+    MaxPool2D(padding='same'),  # 64
+    Conv2D(filters=8, kernel_size=5, activation='relu', padding='same',
+           kernel_initializer='random_uniform',
+           bias_initializer='zeros'
+           ),
+    MaxPool2D(padding='same'),  # 32
+    Conv2D(filters=8, kernel_size=5, activation='relu', padding='same',
+           kernel_initializer='random_uniform',
+           bias_initializer='zeros'
+           ),
+    MaxPool2D(padding='same'),  # 16
+    Conv2D(filters=8, kernel_size=5, activation='relu', padding='same',
+           kernel_initializer='random_uniform',
+           bias_initializer='zeros'
+           ),
+    MaxPool2D(padding='same'),  # 8
+    Conv2D(filters=8, kernel_size=5, activation='relu', padding='same',
+           kernel_initializer='random_uniform',
+           bias_initializer='zeros'
+           ),
+    MaxPool2D(padding='same'),  # 4
+    #Conv2D(filters=512, kernel_size=3, activation='relu', padding='same'),
+    #MaxPool2D(padding='same'),  # 2
+    #Conv2D(filters=1024, kernel_size=3, activation='relu', padding='same'),
+    #MaxPool2D(padding='same'),  # 1
 
     Flatten(),
-    Dense(2, input_shape=(32,), activation='softmax'),
-
+    Dense(1, activation='sigmoid',
+          kernel_initializer='random_uniform',
+          bias_initializer='zeros'),  # activation='softmax'
 ])
-
 if __name__ == '__main__':
-    sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='mean_squared_error', optimizer=sgd)
+    #optimizer = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    optimizer = optimizers.adam(lr=1e-4, decay=1e-6)
+    print(model.weights)
+    model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['accuracy'])
+    print(model.weights)
+
+    print(os.listdir('../ImageGeneration/Images'))
+
+
+    while True:
+        images, labels = ImageGenerator.generator([
+            "../ImageGeneration/Images/WithLicence",
+            "../ImageGeneration/Images/WithoutLicence",
+            "../ImageGeneration/Images/Backgrounds"],
+            tbgenerated=200)
+
+        print(labels)
+        print(np.average(labels))
+
+        model.fit(np.array(images), labels, batch_size=18, epochs=50, verbose=1, validation_split=0.25)
+        model.summary()
+
+
+        def normalized(a, axis=-1, order=2):
+            l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
+            l2[l2 == 0] = 1
+            return a / np.expand_dims(l2, axis)
+
+
+        mw = model.layers[0].get_weights()[0]
+        for k in range(mw.shape[3]):
+            thek = (normalized(mw[:,:,:,k]+1, axis=2))*255 #TODO: testing norm axis=2
+            print(thek)
+            print_picture(Image.frombuffer("RGB", thek.shape[0:2], thek))
+
+        #model.layers[0].get_weights()[0][:,:,:,0]
+        #print_picture(loaded_image=
+        #    Image.fromarray(
+        #        np.asarray(model.layers[0].get_weights()[0])[:,:,0:3,0])
+        #)
+
+
+
+    #print(repr(images))
+
