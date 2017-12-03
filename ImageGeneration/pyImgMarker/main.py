14bd95284bc030ed3cd7d2b07afb4f9a98d1658d
Asger <aweirs15@student.aau.dk>
2017-12-04 00:51:08 +0100

    Restructured Everything in the GenerationFile. Removed the need for at class to keep the images. Reduced the clutter by A LOT!

--- ImageGeneration/pyImgMarker/main.py
+++ ImageGeneration/pyImgMarker/main.py
@@ -1,16 +1,18 @@
-from image_handler import main_imgs
+from ImageGeneration.pyImgMarker.image_handler import main_imgs
 from webbrowser import open
 from traceback import print_exc
+import platform
+from os.path import join
 
-settings_path = "imgSettings.JSON"
-image_root = "images"
 
-main_imgs(settings_path, image_root)
+
+image_root = join("ImageGeneration", "Images", "car")
+settings_path = join(image_root, "imgSettings.JSON")
 
 
 try:
-   pass
+    main_imgs(settings_path, image_root)
 except BaseException as e:
-    open("http://stackoverflow.com/search?q=[python]+" + str(e) , new=2, autoraise=True)
+    open("http://stackoverflow.com/search?q=[python]+" + str(e), new=2, autoraise=True)
     print_exc()
 
