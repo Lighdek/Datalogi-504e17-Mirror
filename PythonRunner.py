e8cf814fc003e9b213e085b3f74d74c2e007bea0
Xiazee <Xiazee@users.noreply.github.com>
2017-12-19 13:55:38 +0100

    moar fixes to match spark

--- PythonRunner.py
+++ PythonRunner.py
@@ -4,6 +4,8 @@ import metrics as m
 from ImageGeneration import ImageLoader
 from KerasModel import Log2ConvolutionalNetwork as theThing
 
+import tensorflow as tf
+
 modelExt = ".hem"
 modelFilename = os.path.join(*theThing.__name__.split('.')) + modelExt
 print(modelFilename)
@@ -25,13 +27,22 @@ if __name__ == '__main__':
     #                                       write_graph=True, write_grads=True, write_images=True, embeddings_freq=0,
     #                                       embeddings_layer_names=None, embeddings_metadata=None, )
 
-    callback = keras.callbacks.CSVLogger("logs/SparkEquivalent", append=True)
+    #callback = keras.callbacks.CSVLogger("logs/SparkEquivalent", append=True)
 
+    writer = tf.summary.FileWriter("logs/sparklike")
     for i in range(50):
-        images, labels = ImageLoader.loadImages(datasets = [(4200, 'GenLicenseOnBackground')]) #35*2*48/.8
+        images, labels = ImageLoader.loadImages(datasets = [(35*2*48, 'GenLicenseOnBackground')])
 
         model.fit(images, labels, batch_size=batchsize, epochs=1, verbose=1,
-              validation_split=0.20, shuffle=True, callbacks=[callback])
+              validation_split=0.0, shuffle=True)#, callbacks=[callback])
+
+        images, labels = ImageLoader.loadImages([(100, "GenLicenseOnBackground")])
+        summary = model.evaluate(images, labels, batch_size=100)
+
+        summaries = tf.Summary(value=[
+            tf.Summary.Value(tag=model.metrics_names[x], simple_value=summary[x]) for x in range(len(model.metrics_names))
+        ])
+        writer.add_summary(summaries)
 
     #model.evaluate(np.array(images), labels, batch_size=50, verbose=1)
 
